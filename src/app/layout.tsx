cf518cec9692c0516c76130e1dc82ea412c72891
Gustavo Brun <gustavobdossantos.gb@gmail.com>
2024-07-09 16:05:18 -0300

    refactor: change default theme to light

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
       <body>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
